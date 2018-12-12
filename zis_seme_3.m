e45cc34ec7688758ffb977576a38216db8897fac
kratochviljakub <37896118+kratochviljakub@users.noreply.github.com>
2018-12-12 09:58:23 +0100

    Finální verze

--- zis_seme_3.m
+++ zis_seme_3.m
@@ -20,33 +20,87 @@ K = (A + [1 0; 0 1])*inv(C);
 r1 = C*Bf;
 r2 = C*K*Df;
 r3 = Df;
-rank([r2 r3])
+rank([r2 r3]);
 
-%% prubehy u, x, y, r, f_u, f_y
+%% Generátor rozhodnutí
 sim('schema_simout.slx')
-figure % vvstup u
+%spodní hranice
+r = simout_y - simout_y_odhad;
+test = r.Data(:,1).^2 + r.Data(:,2).^2;
+chyba = 1e-4;
+for i = 1:size(r.Data(:,1),1)
+   if test(i) <= chyba
+       d(i) = 0; 
+   else
+       d(i) = 1; 
+   end
+end
+figure('Name','fig1');
+plot(simout_y.Time,d)
+title('Generátor rozhodnutí pro \epsilon = 10^{-4}')
+ylabel({'Chyba'});
+xlabel('Time (seconds)');
+hold on
+plot([0 0],[1.5 1.5])
+plot([0 100],[1 1],'r','LineWidth', 1,'LineStyle',':')
+
+%horní hranice
+r = simout_y - simout_y_odhad;
+test = r.Data(:,1).^2 + r.Data(:,2).^2;
+chyba = 1e-11;
+for i = 1:size(r.Data(:,1),1)
+   if test(i) <= chyba
+       d(i) = 0; 
+   else
+       d(i) = 1; 
+   end
+end
+figure('Name','fig2');
+plot(simout_y.Time,d)
+title('Generátor rozhodnutí pro \epsilon = 10^{-11}')
+ylabel({'Chyba'});
+xlabel('Time (seconds)');
+hold on
+plot([0 0],[1.5 1.5])
+plot([0 100],[1 1],'r','LineWidth', 1,'LineStyle',':')
+%% prubehy u, x, y, r, f_u, f_y
+figure('Name','fig3'); % vvstup u
 plot(simout_u)
 title('Vstup y(t)')
 
-figure % stav x
+figure('Name','fig4'); % stav x
 plot(simout_x)
 title('Stav x(t)')
 
-figure % vystup y
+figure('Name','fig5'); % vystup y
 plot(simout_y)
 title('Výstup y(t)')
 
-figure % rezidualni signal r
-r = simout_y - simout_y_odhad
+figure('Name','fig6'); % rezidualni signal r
 plot(r)
-title('Residualni signal r(t)')
+title('Residuální signál r(t)')
 legend('r_1(t)','r_2(t)')
+ylabel('Velikost reziduálního signálu')
 
-figure %chyba f_u
+figure('Name','fig7'); %chyba f_u
 plot(f_u.time, f_u.signals.values)
 title('Chyba akèního èlenu f_u')
+xlabel('Time (seconds)')
+ylabel('Velikost chyby')
 
-figure %chyba f_y
+figure('Name','fig8'); %chyba f_y
 plot(f_y.time, f_y.signals.values)
 title('Chyba senzoru f_y')
+xlabel('Time (seconds)')
+ylabel('Velikost chyby')
 
+%%
+FolderName = tempdir;   % Your destination folder
+FigList = findobj(allchild(0), 'flat', 'Type', 'figure');
+for iFig = 1:length(FigList)
+  FigHandle = FigList(iFig);
+  FigName   = get(FigHandle, 'Name');
+  %savefig(FigHandle, fullfile(FolderName, FigName, '.eps'));
+  set(0, 'CurrentFigure', FigHandle);
+  saveas(FigHandle, fullfile(FolderName, [FigName, '.eps']), 'epsc');
+end
